86cb21cde15904e520a33961f18c8066194f094a
Shreyasi Chowdhury <85079050+ShreyasiChowdhury@users.noreply.github.com>
2023-02-12 18:05:17 +0530

    class assignment

---
+++ ass4/qstn10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class qstn10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row number:");
+        int n = sc.nextInt();
+        int sum = 0, x = n - 1;
+        for (int i = 1; i < n; i++) {
+            sum = sum + i;
+        }
+        x = x + sum;
+        int num = 0;
+        for (int i = 1; i <= n; i++) {
+            num = x;
+            for (int j = 1; j <= n; j++) {
+                if (j <= n - i + 1) {
+                    System.out.print(x + "\t");
+                    x++;
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+            x = num - n + i;
+        }
+    }
+}
